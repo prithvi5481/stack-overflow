765f97b3c5ba86a0f397ddfec46e1c995b1921db
prithvi5481 <prithvisingh91827@gmail.com>
2024-05-16 10:47:41 +0530

    stack overflow clone hosting

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const app = express();
 dotenv.config();
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
-app.use(cors());
+app.use(cors(
+    {
+        origin:['https://deploy'],
+        methods:["POST","GET"],
+        credentials:true,
+    }
+));
 
 app.get('/',(req,res)=>{
     res.send('This is a stack overflow clone API');
@@ -24,6 +30,6 @@ const PORT = process.env.PORT || 5000;
 
 const DATABASE_URL = process.env.CONNECTION_URL
 
-mongoose.connect(DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://prithvisingh91827:prithvi91827@cluster0.ep7cwjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=> app.listen(PORT,()=>{console.log(`server is listening on port ${PORT}`)}))
     .catch((err)=> console.log('unable to connect to mongodb atlas database'));
